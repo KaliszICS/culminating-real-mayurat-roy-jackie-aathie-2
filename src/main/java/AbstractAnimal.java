7a86efa04c47bfc7d52f6353d6c63087987ab196
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-06-08 21:03:07 -0400

    Update AbstractAnimal.java

--- src/main/java/AbstractAnimal.java
+++ src/main/java/AbstractAnimal.java
@@ -2,8 +2,8 @@ public abstract class AbstractAnimal {
   private int id;
   private String name;
   private int age;
-  public void feed();
-  public void makeSound();
-  public void move();
-  public String getInto();
+  public void feed(){};
+  public void makeSound(){};
+  public void move(){};
+  public String getInto(){return "";};
 }
