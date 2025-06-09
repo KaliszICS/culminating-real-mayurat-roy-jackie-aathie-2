4589cbce0dd7dbe13530bb858e0bf1a5af0a9cd6
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-06-08 20:51:26 -0400

    Update Giraffe.java

--- src/main/java/Giraffe.java
+++ src/main/java/Giraffe.java
@@ -1,6 +1,7 @@
 public class Giraffe extends Mammal{
   @Override
-  public void feed(System.out.println("Acaica"));
+  public void feed(){System.out.println("Acaica")}; 
+
   @Override
-  public void makeSound(System.out.println("snort"));
+  public void makeSound(){System.out.println("snort")};
 }
