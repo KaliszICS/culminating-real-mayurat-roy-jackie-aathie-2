13e77acafacf2f62813f9ac955d167009fb03aa9
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-06-08 20:21:15 -0400

    Create AbstractAnimal.java

---
+++ src/main/java/AbstractAnimal.java
@@ -0,0 +1,9 @@
+public abstract AbstractAnimal {
+  private int id;
+  private String name;
+  private int age;
+  public void feed();
+  public void makeSound();
+  public void move();
+  public String getInto();
+}
