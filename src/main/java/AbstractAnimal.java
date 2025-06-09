6221702b0f7952fd3a3a2f1d216a4a7bd596147f
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-06-08 21:21:48 -0400

    Update AbstractAnimal.java

--- src/main/java/AbstractAnimal.java
+++ src/main/java/AbstractAnimal.java
@@ -1,9 +1,36 @@
 public abstract class AbstractAnimal {
-  private int id;
-  private String name;
-  private int age;
-  public void feed(){};
-  public void makeSound(){};
-  public void move(){};
-  public String getInto(){return "";};
+    protected int id;
+    protected String name;
+    protected int age;
+
+    private static int idCounter = 0;
+
+    public AbstractAnimal(String name, int age) {
+        id = idCounter++; 
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void feed();
+    public abstract void makeSound();
+
+    public void move() {
+        System.out.println(name + " is moving.");
+    }
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
