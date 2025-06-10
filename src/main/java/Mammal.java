41e07a34294a0cd16ff4ee12f3c2f7862c1ff047
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-06-10 18:18:11 +0000

    mammal class done

--- src/main/java/Mammal.java
+++ src/main/java/Mammal.java
@@ -1,5 +1,11 @@
-public class Mammal extends AbstractAnimal{
-  private String furType;
-  @Override
-  public void makeSound();
+public abstract class Mammal extends AbstractAnimal {
+    protected String furType;
+
+    public Mammal(int id, String name, int age, String furType) {
+        super(id, name, age);
+        this.furType = furType;
+    }
+
+    public abstract void makeSound();
+    public abstract void feed();
 }
