79b355916c6e6671b34dfbb5383957b2ba5cce0f
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-06-11 13:02:44 -0400

    Update Capybara.java

--- src/main/java/Capybara.java
+++ src/main/java/Capybara.java
@@ -1,37 +1,15 @@
 public class Capybara extends Mammal {
-
 public Capybara(int id, String name, int age) {
-
 super(id, name, age, "Short brown fur");
-
     }
-
-
-
     @Override
 
  public void feed()    
-
  {
-
-       
-
     System.out.println(name + " (Capybara) eats grass.");
-
        }
-
-
-
-         @Override
-
-         
-
+    @Override
     public void makeSound() {
-
-       
-
         System.out.println(name + " (Capybara) squeaks.");
-
     }
-
 }
