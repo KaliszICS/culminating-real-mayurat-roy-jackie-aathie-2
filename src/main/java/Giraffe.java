1a6fc3f402c5d9be445d737ca3f42dcd386143f6
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-06-11 17:46:54 +0000

    fix aathies comments

--- src/main/java/Giraffe.java
+++ src/main/java/Giraffe.java
@@ -2,15 +2,21 @@
  * Giraffe class
  * Inherites from Mammal
  * Giraffes is a type of mammal but a mammal is not a type of giraffe
- * 
+ * @override is not needed but i cant break the habit
  */
 public class Giraffe extends Mammal        
 //a giraffe is a animal with big neck
 {
-
+/*
+ * constructs the giraffe
+ * @param Id: the id of the giraffe
+ * @param name: the name of the giraffe
+ * @param age: the age of the giraffe
+ * see it inherited from abstract animal again
+ */
     public Giraffe(int id, String name, int age) {
 
-super(id, name, age, "Spotted fur");
+super(id, name, age, "Spotted fur"); // used to refer to the superclass inside the parent class
 
    
 
@@ -18,7 +24,10 @@ super(id, name, age, "Spotted fur");
 
     @Override
 
-
+/*
+ * feeding them using abstract
+ * every animal eats something them
+ */
 
         public void feed()  
 
@@ -31,7 +40,9 @@ super(id, name, age, "Spotted fur");
 }
 
 
-
+/* sound using abstract
+ * every animal eats something else
+ */
         @Override
 
     public void makeSound()
